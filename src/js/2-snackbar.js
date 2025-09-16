a689604e9ac661a232f4fbe834cda187f5b66db8
Halynka Obykhvost <fiksikiob@gmail.com>
2025-09-16 18:05:36 +0300

    push cod

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    
+    const delay = parseInt(document.querySelector('[name="delay"]').value);
+    const state = document.querySelector('input[name="state"]:checked').value;
+  
+    
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);  
+        } else {
+          reject(delay);   
+        }
+      }, delay);
+    });
+  
+    
+    promise
+      .then((delay) => {
+        iziToast.success({
+          title: 'Success',
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch((delay) => {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+  });
